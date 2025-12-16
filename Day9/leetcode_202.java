c22dd90270dd074de7915b55063226db8cb768a8
Ajit Pal <ajitpalbbt@gmail.com>
2025-12-16 23:53:18 +0530

    day9

--- Day9/leetcode_202.java
+++ Day9/leetcode_202.java
@@ -1,6 +1,33 @@
 // leetcode 202. Happy Number ?
 
 class Solution {
+    public int sumOfSquares(int n){ // Here we find the Sum of Squares of Numbers
+          int sum = 0;
+        while(n > 0){
+          int digit = n % 10;
+          sum = sum + digit * digit;
+          n = n / 10;
+        }
+          return sum;
+    }
+    
+    public boolean isHappy(int n) {
+       HashSet<Integer> set = new HashSet<>();
+
+         while(n != 1){
+               if(set.contains(n)){
+                   return false;   // if number is repeated and cycle detected
+               }
+             set.add(n);
+             n = sumOfSquares(n);
+         }
+        return true;  // if my number is reached to the 1
+    }
+}
+
+// And here  is the second Approach by slow and fast pointer
+
+class Solution {
     public int sumOfSquares(int n){  // here is the function to find sum of Squares of no
          int sum = 0;
          while(n > 0){
