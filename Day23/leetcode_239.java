168a7925c06ccf8fb273b193660fe8d06a83ca83
Ajit Pal <ajitpalbbt@gmail.com>
2025-12-30 00:15:01 +0530

    day23

--- Day23/leetcode_239.java
+++ Day23/leetcode_239.java
@@ -22,3 +22,34 @@ class Solution {
         return result;
   }
 }
+
+// Optimize Approach Using Sliding Window Approach 
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int ri = 0;
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            
+            if (!dq.isEmpty() && dq.peek() == i - k) {
+                dq.poll();
+            }
+
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            dq.offer(i);
+
+            if (i >= k - 1) {
+                result[ri++] = nums[dq.peek()];
+            }
+        }
+
+        return result;
+    }
+}
