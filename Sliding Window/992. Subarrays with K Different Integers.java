4c38790d5413ad69ec38e11ceaff24e64629ea43
Ajit Pal <ajitpalbbt@gmail.com>
2025-12-31 00:17:39 +0530

    992. Subarrays with K Different Integers.java

--- Sliding Window/992. Subarrays with K Different Integers.java
+++ Sliding Window/992. Subarrays with K Different Integers.java
@@ -1,5 +1,6 @@
 // leetcode 992. Subarrays with K Different Integers ?
 
+// Brute force Approach which gives Time Limit Exceeded
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
         int max = 0;
@@ -21,5 +22,38 @@ class Solution {
                 }
         }
         return max;
+    }
+}
+
+//Optimize Approach Using Sliding Window technique
+class Solution {
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+          return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while(j < nums.length){
+            if(!map.containsKey(nums[j])){
+                 map.put(nums[j], 0);
+            }
+            map.put(nums[j], map.get(nums[j]) + 1);
+
+            while (map.size() > k) {
+                map.put(nums[i], map.get(nums[i]) - 1);
+                if (map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            count = count + j - i + 1;
+            j++;
+        }
+        return count;
     }
 }
