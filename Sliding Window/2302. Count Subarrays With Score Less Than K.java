60584c3ca5117d69f952c44861c39abd6fc3e679
Ajit Pal <ajitpalbbt@gmail.com>
2026-01-01 01:39:51 +0530

    2302. Count Subarrays With Score Less Than K.java

--- Sliding Window/2302. Count Subarrays With Score Less Than K.java
+++ Sliding Window/2302. Count Subarrays With Score Less Than K.java
@@ -20,3 +20,25 @@ class Solution {
         return count;
     }
 }
+
+// Optimize Approach using Sliding Window technique
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long count = 0;
+        int i = 0;
+        int j = 0;
+        long sum = 0;
+
+        while(j < nums.length){
+             sum = sum + nums[j];
+             
+            while(sum * (j - i + 1) >= k){
+                sum = sum - nums[i];
+                   i++;
+             }
+          count = count + (j - i + 1);
+          j++;
+        }
+        return count;
+    }
+}
