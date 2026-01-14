fb6bd835e45eec8f7d8a4f7fa19412d21ae42340
Ajit Pal <ajitpalbbt@gmail.com>
2026-01-14 23:56:17 +0530

    leetcode 143. Reorder List.java

---
+++ Linked List - In-Place Manipulation/Leetcode 143. Reorder List.java
@@ -0,0 +1,38 @@
+// leetcode 143. Reorder List ?
+
+class Solution {
+    public void reorderList(ListNode head) {
+         ListNode slow = head;
+         ListNode fast = head;
+
+         while(fast != null && fast.next != null){
+              slow = slow.next;
+              fast = fast.next.next;
+         }
+
+         ListNode prev = null;
+         ListNode curr = slow;
+
+         while(curr != null){
+              ListNode next = curr.next;
+
+              curr.next = prev;
+              prev = curr;
+              curr = next;
+         }
+
+         ListNode l1 = head;
+         ListNode l2 = prev;
+
+         while(l2.next != null){
+              ListNode l1N = l1.next;
+              l1.next = l2;
+
+              ListNode l2N = l2.next;
+              l2.next = l1N;
+
+              l1 = l1N;
+              l2 = l2N;
+         }
+    }
+}
