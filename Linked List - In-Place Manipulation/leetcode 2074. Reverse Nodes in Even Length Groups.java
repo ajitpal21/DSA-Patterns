e4b5609ca775256dab3a2148c6d94b377293c507
Ajit Pal <ajitpalbbt@gmail.com>
2026-01-19 00:49:58 +0530

    leetcode 2074. Reverse Nodes in Even Length Groups.java

---
+++ Linked List - In-Place Manipulation/leetcode 2074. Reverse Nodes in Even Length Groups.java
@@ -0,0 +1,56 @@
+// leetcode 2074. Reverse Nodes in Even Length Groups ?
+
+class Solution {
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        int len = 0;
+        int probGroupSize = 1;
+
+        ListNode curr = head;
+
+        while(curr != null){
+              len = len + 1;
+              curr = curr.next;
+        }
+
+        curr = head;
+
+        ListNode p = null;
+
+        while(curr != null){
+             int actualGroupSize = Math.min(probGroupSize, len);
+
+             if(actualGroupSize % 2 == 0){
+                ListNode prev = null;
+                int counter = 0;
+                ListNode temp1 = curr;
+
+                while(counter < actualGroupSize){
+                      counter = counter + 1;
+
+                      ListNode next = curr.next;
+
+                      curr.next = prev;
+                      prev = curr;
+                      curr = next;
+                }
+
+                p.next = prev;
+                temp1.next = curr;
+
+                p = temp1;
+             }else{
+                  int counter = 0;
+                  while(counter < actualGroupSize){
+                       counter = counter + 1;
+                       p = curr;
+                       curr = curr.next;
+                  }
+             }
+
+             probGroupSize = probGroupSize + 1;
+             len = len - actualGroupSize;
+        }
+
+        return head;
+    }
+}
